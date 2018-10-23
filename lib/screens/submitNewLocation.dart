44aa68be6068d4ad947892e320ac16cb9f091f39
Libero793 <Louisnell@web.de>
2018-10-23 22:53:53 +0200

    name Location

--- lib/screens/submitNewLocation.dart
+++ lib/screens/submitNewLocation.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:deepblue/models/RegisterLocationModel.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong/latlong.dart';
-
+import 'package:flutter_rating/flutter_rating.dart';
 
 class SubmitNewLocationScreen extends StatefulWidget{
 
@@ -23,12 +23,22 @@ class _SubmitNewLocationScreen extends State<SubmitNewLocationScreen>{
 
   Color menuBackgroundColor = Colors.blue[900];
   
+  
   //_RegisterLocationScreen(this.pushedLocation);
   Future httpReturn;
   String test;
+  FocusNode _focus = new FocusNode();
+  
 
- 
- 
+  @override
+  void initState() {
+    super.initState();
+    _focus.addListener(_onFocusChange);
+  }
+
+  void _onFocusChange(){
+    debugPrint("Focus: "+_focus.hasFocus.toString());
+  }
 
   void httpRequest()async {
 
@@ -67,6 +77,10 @@ class _SubmitNewLocationScreen extends State<SubmitNewLocationScreen>{
     print("fliessend Wasser ${registerModel.getFliessendWasser()}");
     print("motor Waesche ${registerModel.getMotorWaesche()}");
     print("location: $pushedLocation");
+    final theme = Theme.of(context);
+
+    
+
     return Scaffold(
       backgroundColor: menuBackgroundColor,
       appBar: AppBar(
@@ -78,32 +92,77 @@ class _SubmitNewLocationScreen extends State<SubmitNewLocationScreen>{
 
       body: new Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
 
             Row(),
 
+
             Padding(
               padding: const EdgeInsets.fromLTRB(36.0, 40.0, 36.0, 0.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text("Waschbox hinzufügen", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 26.0)),
+                  //Icon(Icons.check_circle_outline,color: Colors.white,size: 30.0,),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                    child:  Text("Waschbox bennen", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 26.0)),
+                    ),
                 ],
               )
             ),
 
+
+
             Padding(
-              padding: const EdgeInsets.fromLTRB(36.0, 10.0, 36.0, 80.0),
-              child: Text("Bitte gib ein paar Details an, über die Waschbox die du hinzufügen möchtest", 
+              padding: const EdgeInsets.fromLTRB(36.0, 10.0, 36.0, 40.0),
+              child: Text("Gib deiner Waschbox die du hinzufügen möchtests einen Namen, damit andere Mitglieder diese eifnacher finden können", 
                         style: TextStyle(fontSize: 14.0, color: Colors.white),
                      ),
             
             ),
 
+            Padding(
+              padding: const EdgeInsets.fromLTRB(36.0, 0.0, 36.0, 80.0),
+              child: new Theme(
+                        data: theme.copyWith(primaryColor: Colors.white,accentColor: Colors.white, hintColor: Colors.white),
+                        child: new TextField(
+                          focusNode: _focus,
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(fontWeight: FontWeight.normal, color: Colors.white, fontSize: 22.0, ),
+                          decoration: new InputDecoration(
+                            hintText: "Waschbox-Name",
+                            hintStyle: TextStyle(color: Colors.indigo[400], fontSize: 20.0,fontWeight: FontWeight.normal),
+                            border: new UnderlineInputBorder(
+                              borderSide: new BorderSide(
+                                color: Colors.red
+                              )
+                            )
+                          ),
+                        ),
+                      ),
+            ),
             
 
+            
 
+            /*
+             new StarRating(
+                            size: 40.0,
+                            rating: rating,
+                            color: Colors.white,
+                            borderColor: Colors.white,
+                            starCount: starCount,
+                            onRatingChanged: (rating) => setState(
+                                  () {
+                                    this.rating = rating;
+                                  },
+                                ),
+                          ),
+            */
+            
+
+            
             new Expanded(
               child: new Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -121,7 +180,8 @@ class _SubmitNewLocationScreen extends State<SubmitNewLocationScreen>{
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          Text("Weiter",style: TextStyle(color: menuBackgroundColor, fontSize: 16.0, fontWeight: FontWeight.bold))
+                          Text("Speichern",style: TextStyle(color: menuBackgroundColor, fontSize: 16.0, fontWeight: FontWeight.bold)),
+                         
                         ],
                         ),
                       )
@@ -130,6 +190,8 @@ class _SubmitNewLocationScreen extends State<SubmitNewLocationScreen>{
                 ],
               )
             )
+            
+
           ],
         ),
       ),
